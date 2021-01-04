86cb189605dfb0c8c500c40beb78d7d499c013c0
Gilmar Sanchez Cuartas <gilmarsancu@gmail.com>
2021-01-04 12:23:03 -0500

    Validacion de formulario y capturar informacion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from './componentes/Header';
-function App() {
-  return (
-    <Fragment>
-      <Header
-          titulo = "Cotizador de prestamos"
-      />
-    </Fragment>
-  );
-}
+import Formulario from './componentes/Formulario';
 
+function App() {
+      //Definir el state
+      const [cantidad, guardarCantidad] = useState(0);
+      const [plazo, guardarPlazo] = useState('');
+      return (
+        <Fragment>
+          <Header
+              titulo = "Cotizador de prestamos"
+          />
+          <div className="container">
+            <Formulario
+              cantidad={cantidad}
+              guardarCantidad={guardarCantidad}
+              plazo={plazo}
+              guardarPlazo={guardarPlazo}
+            />
+          </div>
+        </Fragment>
+      );
+}
 export default App;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,5 +3,4 @@ import React from 'react'
 const Header = ({titulo}) => (  
         <h1>{titulo}</h1>
 );
-
 export default Header;
